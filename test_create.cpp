9feb750aab6f06e03ec0ab55ee900945ce26a822
Byelor <zaqw1234bmbmbm@gmail.com>
2026-03-04 14:56:55 +0300

    delete-useless

--- test_create.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-int main() {
-    std::cout << "Writing python input simulation script." << std::endl;
-}
--- test_create.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-import time
-
-print("Compiling test code with an artificial event injection if needed, but since it's hard without display, let's just inspect the source logic heavily.")
